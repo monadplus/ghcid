a03e8b72bffbe1b91a88089ed46c3c7d2b592ef1
Arnau Abella <arnauabella@gmail.com>
2020-10-25 20:07:03 +0100

    add support for single *.hs files

--- ghcid.el
+++ ghcid.el
@@ -25,11 +25,7 @@
 ;; TODO
 ;; - Always loads from buffer directory, it should load from the .cabal's directory.
 
-;; Set ghcid-target to change the target
-(setq ghcid-target "")
-
 (setq ghcid-process-name "ghcid")
-
 
 (define-minor-mode ghcid-mode
   "A minor mode for ghcid terminals
@@ -84,15 +80,17 @@ recognize the new height until you manually restart it by calling
 (defun ghcid-buffer-name ()
   (concat "*" ghcid-process-name "*"))
 
+(defun ghcid-ghci-cmd (target)
+  (format "ghci %s" target))
+
 (defun ghcid-stack-cmd (target)
   (format "stack ghci %s --test --bench --ghci-options=-fno-code" target))
 
 (defun ghcid-cabal-cmd (target)
   (format "cabal repl %s --enable-tests --enable-benchmarks" target))
 
-;; TODO Pass in compilation command like compilation-mode
-(defun ghcid-command (height)
-    (format "ghcid -c \"%s\" -h %s\n" (ghcid-cabal-cmd ghcid-target) height))
+(defun ghcid-command (cmd height)
+    (format "ghcid -c \"%s\" -h %s\n" cmd height))
 
 (defun ghcid-get-buffer ()
   "Create or reuse a ghcid buffer with the configured name and
@@ -102,7 +100,7 @@ User configuration will influence where the buffer gets shown
 exactly. See `ghcid-mode'."
   (display-buffer (get-buffer-create (ghcid-buffer-name)) '((display-buffer-reuse-window))))
 
-(defun ghcid-start (dir)
+(defun ghcid-start (mode filename dir)
   "Start ghcid in the specified directory"
 
   (with-selected-window (ghcid-get-buffer)
@@ -112,8 +110,14 @@ exactly. See `ghcid-mode'."
 
     ;; Only now we can figure out the height to pass along to the ghcid process
     ;; (let ((height (- (window-body-size) 5)))
-    (let ((height (- (window-height) scroll-margin 3)))
+    (let ((height (- (window-height) scroll-margin 4))
+          (ghcid-cmd (nth (- mode 1) '(ghcid-ghci-cmd ghcid-cabal-cmd ghcid-stack-cmd))))
 
+      (setq-local ghcid-target (if (= mode 1) filename ""))
+      ;; TODO this doesn't work
+      ;; (when (/= mode 1)
+      ;;   (message "Target: ")
+      ;;   (read ghcid-target))
       (term-mode)
       (term-line-mode)  ;; Allows easy navigation through the buffer
       (ghcid-mode)
@@ -126,7 +130,7 @@ exactly. See `ghcid-mode'."
            ghcid-process-name
            "/bin/bash"
            nil
-           (list "-c" (ghcid-command height)))
+           (list "-c" (ghcid-command (funcall ghcid-cmd ghcid-target) height)))
 
       )))
 
@@ -140,13 +144,15 @@ exactly. See `ghcid-mode'."
         ))))
 
 ;; TODO Close stuff if it fails
-(defun ghcid ()
+(defun ghcid (mode)
   "Start a ghcid process in a new window. Kills any existing sessions.
-
 The process will be started in the directory of the buffer where
 you ran this command from."
-  (interactive)
-  (ghcid-start default-directory))
+  (interactive "n(1) ghci (2) cabal (3) stack ")
+  (let ((filename (buffer-file-name)))
+    (if (or (< mode 1) (> mode 3))
+        (error! "Wrong mode. Please choose (1), (2) or (3)")
+      (ghcid-start mode filename default-directory))))
 
 ;; Assumes that only one window is open
 (defun ghcid-stop ()
@@ -154,7 +160,6 @@ you ran this command from."
   (interactive)
   (ghcid-kill)
   (kill-buffer (ghcid-buffer-name)))
-
 
 (provide 'ghcid)
 
